390734a052cdf6ac98bccd753bc872ac3059686c
wuyiyan <308111394@qq.com>
2019-05-26 10:37:24 +0800

    添加loading状态

--- src/button.vue
+++ src/button.vue
@@ -1,6 +1,7 @@
 <template>
     <button class="g-button" :class="{[`icon-${iconPosition}`]:true}">
       <g-icon v-if="icon" class="icon" :name="icon"></g-icon>
+      <g-icon class="loading" name="loading"></g-icon>
       <div class="content">
         <slot></slot>
       </div> 
@@ -21,6 +22,10 @@
   }
 </script>
 <style lang="scss">
+  @keyframes spin {
+    0% {transform: rotate(0deg);}
+    100% {transform: rotate(360deg);}
+  }
   .g-button {
       font-size:  var(--font-size);
       height: var(--button-height);
@@ -39,6 +44,7 @@
         > .icon { order: 2; margin-left: 0.43em; margin-right: 0;}
         > .content { order: 1;}
       }
+      .loading { animation: spin 2s infinite linear ;}
   }
 </style>
 
