5c7c5c603e0f442aac71cb20dc4693558c7f6acb
wuyiyan <308111394@qq.com>
2019-05-26 16:58:22 +0800

    初步创建button-group

---
+++ src/button-group.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="g-button-group">
+        <slot></slot>
+    </div>
+</template>
+<script>
+export default {}
+</script>
+<style lang="scss">
+    .g-button-group {
+        display: inline-flex;
+        vertical-align: middle;
+        > .g-button {
+            border-radius: 0;
+            margin-left: -1px;
+            &:first-child {
+                border-top-left-radius: 4px;
+                border-bottom-left-radius: 4px;
+            }
+            &:last-child {
+                border-top-right-radius: 4px;
+                border-bottom-right-radius: 4px;
+            }
+            &:hover {
+                position: relative;
+                z-index: 1;
+            }
+        }
+    }
+    
+</style>
--- src/button.vue
+++ src/button.vue
@@ -32,23 +32,17 @@
     100% {transform: rotate(360deg);}
   }
   .g-button {
-      font-size:  var(--font-size);
-      height: var(--button-height);
-      padding: 0 1em;
-      border-radius: var(--border-radius);
-      border: 1px solid var(--border-color);
-      background: var(--button-bg);
-      display: inline-flex; justify-content: center; align-items: center;
-      vertical-align: middle;
+      font-size:  var(--font-size); height: var(--button-height); padding: 0 1em;
+      border-radius: var(--border-radius); border: 1px solid var(--border-color);
+      background: var(--button-bg); display: inline-flex; justify-content: center;
+      align-items: center; vertical-align: middle;
       > svg { margin-top: 1.5px; }
       &:hover { border-color: var(--border-color-hover);}
       &:active { background-color: var(--button-active-bg);}
       &:focus { outline: none;}
-      > .icon { order: 1; margin-right: 0.43em;}
-      > .content { order: 2;}
+      > .icon { order: 1; margin-right: 0.43em;} > .content { order: 2;}
       &.icon-right{
-        > .icon { order: 2; margin-left: 0.43em; margin-right: 0;}
-        > .content { order: 1;}
+        > .icon { order: 2; margin-left: 0.43em; margin-right: 0;} > .content { order: 1;}
       }
       .loading { animation: spin 2s infinite linear ;}
   }
