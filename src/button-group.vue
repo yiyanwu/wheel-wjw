bf064c92e4ea27c1f2c878187225a6ea9aedcf20
wuyiyan <308111394@qq.com>
2019-05-26 17:14:28 +0800

    添加 检测group的子元素是不是button 的功能

--- src/button-group.vue
+++ src/button-group.vue
@@ -4,7 +4,17 @@
     </div>
 </template>
 <script>
-export default {}
+export default {
+    mounted () {
+        for(let node of this.$el.children){
+            let name = node.nodeName.toLowerCase()
+            if (name !== 'button'){
+                console.warn(`g-button-group 的子元素应该全是 g-button ，
+                但是你写的是 ${name} ，这样会引起样式上的错误，如果你不在乎，就忽略这个警告`)
+            }
+        }
+    }
+}
 </script>
 <style lang="scss">
     .g-button-group {
