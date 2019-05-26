52ccc48a70cbd03ff7d3dc19fac71fddd821226c
wuyiyan <308111394@qq.com>
2019-05-26 16:23:35 +0800

    添加loading和loading的click时间

--- src/button.vue
+++ src/button.vue
@@ -1,7 +1,8 @@
 <template>
-    <button class="g-button" :class="{[`icon-${iconPosition}`]:true}">
-      <g-icon v-if="icon" class="icon" :name="icon"></g-icon>
-      <g-icon class="loading" name="loading"></g-icon>
+    <button class="g-button" :class="{[`icon-${iconPosition}`]:true}"
+    @click="$emit('click')">
+      <g-icon class="icon" v-if="icon && !loading" :name="icon"></g-icon>
+      <g-icon class="loading icon" v-if="loading" name="loading"></g-icon>
       <div class="content">
         <slot></slot>
       </div> 
@@ -11,6 +12,10 @@
   export default {
     props : {
       icon : {},
+      loading : {
+        type : Boolean,
+        default : false
+      },
       iconPosition : {
         type : String,
         default : 'left',
