77629493824e0e99e68fd98ea9740018b5b67ba9
wuyiyan <308111394@qq.com>
2019-05-27 14:00:21 +0800

    使用chai.expect添加四个测试用例，第五个click需要用其他方法

--- src/app.js
+++ src/app.js
@@ -15,3 +15,76 @@ new Vue({
         loading3 : false
     }
 })
+
+import chai from 'chai'
+const expect = chai.expect
+//单元测试
+{
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+        propsData : {
+            icon : 'settings'
+        }
+    })
+    vm.$mount()
+    let useElement = vm.$el.querySelector('use')
+    expect(useElement.getAttribute('xlink:href')).to.eq('#i-settings')
+    vm.$el.remove()
+    vm.$destroy()
+}
+{
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+        propsData : {
+            icon : 'settings',
+            loading : true
+        }
+    })
+    vm.$mount()
+    let useElement = vm.$el.querySelector('use')
+    expect(useElement.getAttribute('xlink:href')).to.eq('#i-loading')
+    vm.$el.remove()
+    vm.$destroy()
+}
+{
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+        propsData : {
+            icon : 'settings'
+        }
+    })
+    vm.$mount(div)
+    let svg = vm.$el.querySelector('svg')
+    expect(window.getComputedStyle(svg).order).to.eq('1')
+    vm.$el.remove()
+    vm.$destroy()
+}
+{
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+        propsData : {
+            icon : 'settings',
+            iconPosition : 'right'
+        }
+    })
+    vm.$mount(div)
+    let svg = vm.$el.querySelector('svg')
+    expect(window.getComputedStyle(svg).order).to.eq('2')
+    vm.$el.remove()
+    vm.$destroy()
+}
+{
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+        propsData : {
+            icon : 'settings'
+        }
+    })
+    vm.$mount()
+    vm.$on('click',function () {})
+    let button = vm.$el
+}
