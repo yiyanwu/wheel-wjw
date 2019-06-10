e742db2b45634d1e2423e4c79b383ea1facc8118
wuyiyan <308111394@qq.com>
2019-06-10 20:16:17 +0800

    添加 row 的left/right/center 属性

--- src/col.vue
+++ src/col.vue
@@ -41,8 +41,6 @@ export default {
 </script>
 <style scoped lang="scss">
     .col{
-            width: 50%;
-
             $class-prefix: col-;
             @for $n from 1 through 24 {
                 &.#{$class-prefix}#{$n} {
--- src/row.vue
+++ src/row.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="row" :style="{rowStyle}">
+    <div class="row" :style="rowStyle" :class="rowClass">
         <slot></slot>
     </div>
 </template>
@@ -9,15 +9,22 @@ export default {
     props: {
         gutter: {
             type: [Number,String]
+        },
+        align: {
+            type: String,
+            validator (value) {
+                return ['left','right','center'].includes(value)
+            }
         }
     },
     computed: {
+        rowClass () {
+            let {align} = this
+            return [align && `align-${align}`]
+        },
         rowStyle () {
             let {gutter} = this
-            return {
-                marginLeft: -gutter/2 + 'px' ,
-                marginRight: -gutter/2 + 'px'
-            }
+            return { marginLeft: -gutter/2 + 'px' , marginRight: -gutter/2 + 'px'}
         }
     },
     created () {},
@@ -29,5 +36,16 @@ export default {
 }
 </script>
 <style scoped lang="scss">
-     .row { display: flex; }
+     .row { 
+         display: flex;
+         &.align-left {
+             justify-content: flex-start;
+         }
+         &.align-right {
+             justify-content: flex-end;
+         }
+         &.align-center {
+             justify-content: center;
+         }
+    }
 </style>
