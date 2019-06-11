1bfebbf02aec4a38b51a0aaae07c66098db69f89
wuyiyan <308111394@qq.com>
2019-06-11 16:16:11 +0800

    添加sider的简单动画

--- src/sider.vue
+++ src/sider.vue
@@ -1,15 +1,34 @@
 <template>
-    <div class="sider">
+  <transition name="slide">
+    <div class="sider" v-if="visible">
         <slot></slot>
+        <button @click="visible = false">close</button>
     </div>
+  </transition>
 </template>
 <script>
 export default {
-    name: 'WheelSider'
+    name: 'WheelSider',
+    data() {
+        return {
+            visible:true
+        }
+    }
 }
 </script>
 <style scoped lang="scss">
     .sider {
-        
+        position: relative;
+        > button {
+            position: absolute;
+            top: 0;
+            right: 0;
+        }
+    }
+    .slide-enter-active, .slide-leave-active {
+        transition: all 1s;
+    }
+    .slide-enter, .slide-leave-to {
+        margin-left: -200px;
     }
 </style>
