a9faf4cbf959175af573e462dade6bba664fd998
wuyiyan <308111394@qq.com>
2019-06-14 14:32:03 +0800

    优化三种动画和弹框样式

--- src/toast.vue
+++ src/toast.vue
@@ -1,12 +1,14 @@
 <template>
-  <div class="toast" ref="wrapper" :class="toastClasses">
-    <div class="message">
-      <slot v-if="!enableHtml"></slot>
-      <div v-else v-html="$slots.default[0]"></div>
-    </div>
+  <div class="wrapper" :class="toastClasses">
+    <div class="toast" ref="toast">
+      <div class="message">
+        <slot v-if="!enableHtml"></slot>
+        <div v-else v-html="$slots.default[0]"></div>
+      </div>
 
-    <div class="line" ref="line"></div>
-    <span class="close" v-if="closeButton" @click="onClickClose">{{closeButton.text}}</span>
+      <div class="line" ref="line"></div>
+      <span class="close" v-if="closeButton" @click="onClickClose">{{closeButton.text}}</span>
+    </div>
   </div>
 </template>
 <script>
@@ -36,9 +38,9 @@ export default {
     },
     position: {
       type: String,
-      default: 'top',
-      validator (value) {
-        return ['top','bottom','middle'].indexOf(value) >= 0
+      default: "top",
+      validator(value) {
+        return ["top", "bottom", "middle"].indexOf(value) >= 0;
       }
     }
   },
@@ -51,14 +53,14 @@ export default {
     toastClasses() {
       return {
         [`position-${this.position}`]: true
-      }
+      };
     }
   },
   methods: {
     setLineHeight() {
       this.$nextTick(() => {
         this.$refs.line.style.height = `${
-          this.$refs.wrapper.getBoundingClientRect().height
+          this.$refs.toast.getBoundingClientRect().height
         }px`;
       });
     },
@@ -71,7 +73,7 @@ export default {
     },
     close() {
       this.$el.remove();
-      this.$emit('close')
+      this.$emit("close");
       this.$destroy();
     },
     onClickClose() {
@@ -88,22 +90,68 @@ $font-size: 14px;
 $toast-min-height: 40px;
 $toast-bg: rgba(0, 0, 0, 0.75);
 @keyframes fade-in {
-  0% {opacity: 0; transform: translateY(100%);}
-  100% {opacity: 1; transform:  translateY(0%)}
+  0% { opacity: 0; }
+  100% { opacity: 1; }
+}
+@keyframes slide-up {
+  0% { opacity: 0; transform: translateY(100%); }
+  100% { opacity: 1; transform: translateY(0%); }
+}
+@keyframes slide-down {
+  0% { opacity: 0; transform: translateY(-100%); }
+  100% { opacity: 1; transform: translateY(0%); }
+}
+.wrapper {
+  position: fixed;
+  left: 50%;
+  transform: translateX(-50%);
+  $animation-duraction: 400ms;
+  &.position-top {
+    top: 0;
+    .toast {
+      border-top-left-radius: 0;
+      border-top-right-radius: 0;
+      animation: slide-down $animation-duraction;
+    }
+  }
+  &.position-middle {
+    top: 50%;
+    transform: translate(-50%, -50%);
+    .toast {
+      animation: fade-in $animation-duraction;
+    }
+  }
+  &.position-bottom {
+    bottom: 0;
+    .toast {
+      border-bottom-left-radius: 0;
+      border-bottom-right-radius: 0;
+      animation: slide-up $animation-duraction;
+    }
+  }
 }
 .toast {
-  animation: fade-in 1s;
-  position: fixed; left: 50%;
-  font-size: $font-size; color: white; line-height: 1.8;
+  font-size: $font-size;
+  color: white;
+  line-height: 1.8;
   min-height: $toast-min-height;
-  display: flex; align-items: center;
-  background: $toast-bg; box-shadow: 0 0 3px 0 rgba(0, 0, 0, 0.5);
-  padding: 0 16px; border-radius: 4px;
-  .message { padding: 8px 0; }
-  .line { border-left: 1px solid #666; height: 100%; margin-left: 16px; }
-  .close { padding-left: 16px; flex-shrink: 0; }
-  &.position-top { top:0; transform: translateX(-50%);}
-  &.position-middle { top:50%; transform: translate(-50%, -50%);}
-  &.position-bottom { bottom:0; transform: translateX(-50%);}
+  display: flex;
+  align-items: center;
+  background: $toast-bg;
+  box-shadow: 0 0 3px 0 rgba(0, 0, 0, 0.5);
+  padding: 0 16px;
+  border-radius: 4px;
+  .message {
+    padding: 8px 0;
+  }
+  .line {
+    border-left: 1px solid #666;
+    height: 100%;
+    margin-left: 16px;
+  }
+  .close {
+    padding-left: 16px;
+    flex-shrink: 0;
+  }
 }
 </style>
