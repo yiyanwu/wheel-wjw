a8725fa1d4fa363e77d1d1511e8dcc44c7fc0c6b
wuyiyan <308111394@qq.com>
2019-06-16 16:34:28 +0800

    完善tab的样式

--- src/tabs-pane.vue
+++ src/tabs-pane.vue
@@ -34,8 +34,9 @@ export default {
 </script>
 <style scoped lang="scss">
     .tabs-pane {
+        padding: 1em;
         &.active {
-            background:red; 
+           
         }
     }
 </style>
