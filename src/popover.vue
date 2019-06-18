204e9750b10c2caa1e2e933d6e02d7e970394df2
wuyiyan <308111394@qq.com>
2019-06-18 16:32:55 +0800

    更新contentWrapper样式

--- src/popover.vue
+++ src/popover.vue
@@ -3,7 +3,7 @@
     <div ref="contentWrapper" class="content-wrapper" v-if="visible">
       <slot name="content"></slot>
     </div>
-    <span ref="triggerWrapper">
+    <span ref="triggerWrapper" style="display:inline-block;">
       <slot></slot>
     </span>
   </div>
@@ -51,6 +51,8 @@ export default {
 };
 </script>
 <style scoped lang="scss">
+$border-color: #333;
+$border-radius: 4px;
 .popover {
   display: inline-block;
   vertical-align: top;
@@ -58,8 +60,29 @@ export default {
 }
 .content-wrapper {
   position: absolute;
-  border: 1px solid red;
+  border: 1px solid $border-color;
   box-shadow: 0 0 3px 0 rgba(0, 0, 0, 0.5);
   transform: translateY(-100%);
+  margin-top: -10px;
+  padding: .5em 1em;
+  border-radius: $border-radius;
+  &::before,&::after {
+      content: '';
+      border:10px solid transparent;
+      border-top-color:black;
+      display: block;
+      width: 0;
+      height: 0;
+      position: absolute;
+      top:100%;
+      left: 10px;
+  }
+  &::before {
+      border-top-color:black;
+  }
+  &::after {
+      border-top-color:white;
+      top:calc(100% - 1px);
+  }
 }
 </style>
