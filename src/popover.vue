2fd670fb29ea946438b169ed6f27cb062c131097
wuyiyan <308111394@qq.com>
2019-06-18 14:36:50 +0800

    发现一个bug

--- src/popover.vue
+++ src/popover.vue
@@ -1,54 +1,64 @@
 <template>
-  <div class="popover" @click="xxx">
+  <div class="popover" @click="onClick">
     <div ref="contentWrapper" class="content-wrapper" v-if="visible">
-        <slot name="content"></slot>
+      <slot name="content"></slot>
     </div>
     <span ref="triggerWrapper">
-        <slot></slot>
+      <slot></slot>
     </span>
   </div>
 </template>
 <script>
-  export default {
-      name: 'WheelPopover',
-      data () {
-          return { visible: false}
-      },
-      mounted () {
-         
-      },
-      methods: {
-          xxx () {
-              this.visible = !this.visible
-              if (this.visible === true) {
-                  setTimeout(() => {
-                      document.body.appendChild(this.$refs.contentWrapper)
-                      let {width,height,top,left} = this.$refs.triggerWrapper.getBoundingClientRect()
-                      this.$refs.contentWrapper.style.left = left + window.scrollX + 'px'
-                      this.$refs.contentWrapper.style.top = top + window.scrollY + 'px'
+export default {
+  name: "WheelPopover",
+  data() {
+    return { visible: false };
+  },
+  mounted() {},
+  methods: {
+    positionContent () {
+        document.body.appendChild(this.$refs.contentWrapper);
+        let { width, height, top, left} = this.$refs.triggerWrapper.getBoundingClientRect();
+        this.$refs.contentWrapper.style.left = left + window.scrollX + "px";
+        this.$refs.contentWrapper.style.top = top + window.scrollY + "px";
+    },
+    onClick(event) {
+      if (this.$refs.triggerWrapper.contains(event.target)) {
+        this.visible = !this.visible;
+        if (this.visible === true) {
+          setTimeout(() => {
+            this.positionContent()
 
-                      let eventHandler = () => {
-                          this.visible = false
-                          document.removeEventListener('click',eventHandler)
-                      }
-
-                      document.addEventListener('click', eventHandler)
-                  })
+            let eventHandler = (e) => {
+                console.log(e.target)
+                console.log(this.$refs.contentWrapper)
+                console.log(this.visible)
+              if (this.$refs.contentWrapper.contains(e.target)) {
+                  
+              } else {
+                  this.visible = false;
+                  document.removeEventListener("click", eventHandler);
               }
-          }
+            };
+
+            document.addEventListener("click", eventHandler);
+          });
+        } else {}
       }
-  };
+    }
+  }
+};
 </script>
 <style scoped lang="scss">
-  .popover {
-      display: inline-block;
-      vertical-align: top;
-      position: relative;
-  }
-  .content-wrapper {
-      position: absolute;
-      border: 1px solid red;
-      box-shadow: 0 0 3px 0 rgba(0,0,0,.5);
-      transform: translateY(-100%);
-  }
+.popover {
+  display: inline-block;
+  vertical-align: top;
+  position: relative;
+}
+.content-wrapper {
+  position: absolute;
+  border: 1px solid red;
+  box-shadow: 0 0 3px 0 rgba(0, 0, 0, 0.5);
+  transform: translateY(-100%);
+}
 </style>
