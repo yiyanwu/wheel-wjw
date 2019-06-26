2d5664a483e822f5d9c7e277f449f758f235af79
wuyiyan <308111394@qq.com>
2019-06-26 22:00:24 +0800

    更新layout sider

--- src/sider.vue
+++ src/sider.vue
@@ -2,36 +2,14 @@
   <transition name="slide">
     <div class="sider">
         <slot></slot>
-        <!-- <button @click="visible = false">close</button> -->
     </div>
   </transition>
 </template>
 <script>
 export default {
-    name: 'WheelSider',
-    // data() {
-    //     return {
-    //         visible:true
-    //     }
-    // }
+    name: 'WheelSider'
 }
 </script>
 <style scoped lang="scss">
-    // .sider {
-    //     position: relative;
-    //     > button {
-    //         position: absolute;
-    //         top: 0;
-    //         right: 0;
-    //         height: 32px;
-    //         border-radius: 4px;
-    //         background: #e9e7ef;
-    //     }
-    // }
-    // .slide-enter-active, .slide-leave-active {
-    //     transition: all .5s;
-    // }
-    // .slide-enter, .slide-leave-to {
-    //     margin-left: -200px;
-    // }
+     .sider {}
 </style>
