5cbe46b04c45b97b5426b9bd5986c7df4d0019a7
wuyiyan <308111394@qq.com>
2019-06-26 18:55:18 +0800

    完成官网popover部分

---
+++ docs/.vuepress/components/popover-demos-1.vue
@@ -0,0 +1,65 @@
+<template>
+    <div class="container" style="padding: 20px 0;">
+        <g-popover position="top">
+            <g-button>click激活</g-button>
+            <template slot="content">这是一段内容</template>
+        </g-popover>
+        <g-popover position="bottom">
+            <g-button>click激活</g-button>
+            <template slot="content">这是一段内容</template>
+        </g-popover>
+        <g-popover position="left" trigger="hover">
+            <g-button>hover激活</g-button>
+            <template slot="content">这是一段内容</template>
+        </g-popover>
+        <g-popover position="right" trigger="hover">
+            <g-button>hover激活</g-button>
+            <template slot="content">这是一段内容</template>
+        </g-popover>
+        
+        </br>
+        <span @click="show = !show" class="code">展示代码</span>
+        <span v-if="show"><pre><code>{{content}}</code></pre></span>
+    </div>
+</template>
+<script>
+import Button from '../../../src/button'
+import Popover from '../../../src/popover'
+
+
+export default {
+    components: {
+        "g-button": Button,
+        'g-popover':Popover
+    },
+    data(){
+        return {
+            content:`
+            <g-popover position="top">
+            <g-button>click激活</g-button>
+            <template slot="content">这是一段内容</template>
+        </g-popover>
+        <g-popover position="bottom">
+            <g-button>click激活</g-button>
+            <template slot="content">这是一段内容</template>
+        </g-popover>
+        <g-popover position="left" trigger="hover">
+            <g-button>hover激活</g-button>
+            <template slot="content">这是一段内容</template>
+        </g-popover>
+        <g-popover position="right" trigger="hover">
+            <g-button>hover激活</g-button>
+            <template slot="content">这是一段内容</template>
+        </g-popover>
+            `.replace(/^ {8}/gm, '').trim(),
+            show:false
+        }
+    }
+}
+</script>
+<style scoped>
+ .code { 
+     color:#409eff;
+     cursor: pointer;
+ }
+</style>
--- docs/components/popover.md
+++ docs/components/popover.md
@@ -1,3 +1,20 @@
 ---
 title: Popover 弹出框
 ---
+
+# Popover 弹出框
+
+# 基本用法
+
+四种方位，两种触发方式
+
+<ClientOnly>
+ <popover-demos-1></popover-demos-1>
+</ClientOnly>
+
+# Options
+
+参数|说明|类型|可选值|默认值
+---|:--:|---:|---:|---:
+position|控制弹出的方向|String|'top','bottom','left'm'right'|'top'
+trigger|控制激活方式|String|'click','hover'|'click'
