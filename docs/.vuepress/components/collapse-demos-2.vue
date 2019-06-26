88a28be2d2bc7a9a3c8ae2f57a44de94c76171fa
wuyiyan <308111394@qq.com>
2019-06-26 19:52:09 +0800

    完成官网collapse部分

---
+++ docs/.vuepress/components/collapse-demos-2.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="container" style="padding: 20px 0;">
+        <g-collapse :selected.sync="selectedTab" single>
+            <g-collapse-item title="文学" name="1">文学相关</g-collapse-item>
+            <g-collapse-item title="体育" name="2">体育相关</g-collapse-item>
+            <g-collapse-item title="财经" name="3">财经相关</g-collapse-item>
+        </g-collapse>
+
+        </br>
+        <span @click="show = !show" class="code">展示代码</span>
+        <span v-if="show"><pre><code>{{content}}</code></pre></span>
+    </div>
+</template>
+<script>
+import Collapse from '../../../src/collapse'
+import CollapseItem from '../../../src/collapse-item'
+
+export default {
+    components: {
+        'g-collapse': Collapse,
+        'g-collapse-item': CollapseItem
+    },
+    data(){
+        return {
+            content:`
+            data:{ selectedTab:['2'] }
+
+        <g-collapse :selected.sync="selectedTab" single>
+            <g-collapse-item title="文学" name="1">文学相关</g-collapse-item>
+            <g-collapse-item title="体育" name="2">体育相关</g-collapse-item>
+            <g-collapse-item title="财经" name="3">财经相关</g-collapse-item>
+        </g-collapse>
+            `.replace(/^ {8}/gm, '').trim(),
+            show:false,
+            selectedTab:['2']
+        }
+    }
+}
+</script>
+<style scoped>
+ .code { 
+     color:#409eff;
+     cursor: pointer;
+ }
+</style>
--- docs/components/collapse.md
+++ docs/components/collapse.md
@@ -1,3 +1,30 @@
 ---
 title: Collapse 折叠面板
 ---
+
+# Collapse 折叠面板
+
+# 基本用法
+
+默认打开第二项（可选）
+
+<ClientOnly>
+ <collapse-demos-1></collapse-demos-1>
+</ClientOnly>
+
+# 手风琴效果
+
+每次只打开一个
+
+<ClientOnly>
+ <collapse-demos-2></collapse-demos-2>
+</ClientOnly>
+
+# Options
+
+参数|说明|类型|可选值|默认值
+---|:--:|---:|---:|---:
+single|collapse属性,是否开启手风琴效果|Boolean|---|false
+selected|collapse属性，控制默认打开某项|Array|推荐在name中取值|---
+title|标题名称，为必须值|String|---|---
+name|标题索引，为必须值|String|---|---
